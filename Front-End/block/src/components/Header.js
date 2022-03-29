08b17cd0396a2bc1727bd734a4f9ae967404788e
LimaGustav <lima.gustavo100@hotmail.com>
2022-03-29 11:16:00 -0300

    Header responsivo

--- Front-End/block/src/components/Header.js
+++ Front-End/block/src/components/Header.js
@@ -3,19 +3,20 @@ import { useState, useEffect } from "react"
 import { Link } from 'react-router-dom'
 
 import logo from '../assets/block_logo.png'
+import "./css/header.css"
 
 
 export default function Header() {
     return (
         <div>
-            <nav>
+            <nav className='header_nav grid'>
                 <img src={logo} alt="Block time logo" />
 
-                <ul>
+                <ul className='nav_links'>
                     <li><Link to='/administrador'>Administrador</Link></li>
-                    <li><Link to='/clientes'>Administrador</Link></li>
+                    <li><Link to='/clientes'>Clientes</Link></li>
                     <li><Link to='/dashboard'>Dashboard</Link></li>
-                    <li><Link to='/'>Logout</Link></li>
+                    <li><Link className='fa fa-sign-out fa-2x' to='/'></Link></li>
                 </ul>
             </nav>
         </div>
--- Front-End/block/src/pages/ListagemClientes.js
+++ Front-End/block/src/pages/ListagemClientes.js
@@ -3,10 +3,21 @@ import { useState, useEffect } from "react"
 import Header from '../components/Header'
 import api from "../services/api"
 
+import './css/listagemClientes.css'
+
 export default function ListagemClientes() {
     return (
         <div>
             <Header/>
+
+            <div className='cl_container'>
+                <div className='cl_table_head'>
+                    <span>Cliente</span>
+                    <span>Descricao</span>
+                    <span>Status</span>
+                    <span>Contato</span>
+                </div>
+            </div>
         </div>
     )
 }
