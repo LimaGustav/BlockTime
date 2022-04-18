d5eda188de48b6604258d71589debc1f4e619c3c
LimaGustav <lima.gustavo100@hotmail.com>
2022-04-18 11:11:26 -0300

    Arquivo JSON scripts

--- Front-End/block/src/pages/TelaCliente.js
+++ Front-End/block/src/pages/TelaCliente.js
@@ -13,11 +13,14 @@ const Telacliente = () => {
                     <h1 className='p_title'>SypressTech</h1>
                     <i class="fas fa-edit fa-1x"></i>
                 </div>
-                <span>Empresa especializada em desenvolvimento web</span>
-
-                <span>Problema - Severidade</span>
-                <input type="text" />
-                <button type='submit' className='lgn_btn cl_btn'>Testar</button>
+                <div className='p_container_text'>
+                    <p className='p_company_description'>Empresa especializada em desenvolvimento web</p>
+                    <div className='p_container_problem'>
+                        <p>Problema - Severidade</p>
+                        <input readOnly className='p_input' type="text" />
+                    </div>
+                </div>
+                <button type='submit' className='lgn_btn cl_btn p_btn'>Testar</button>
             </div>
 
         </div>
